3ff22ea29260f5f37a1884d4a30e9c63abaece6a
Alex Pizarro <alex.pizarro.j@gmail.com>
2015-05-27 22:38:05 -0400

    Trie optimizations

--- strings/Trie.cpp
+++ strings/Trie.cpp
@@ -1,17 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <size_t alphabet_size>
+template <int alphabet_size>
 struct TrieNode {
-  size_t n_words, n_prefixes;
-  size_t child[alphabet_size];
+  int n_words, n_prefixes;
+  int child[alphabet_size] = {0};
 
-  TrieNode() : n_words(0), n_prefixes(0) { memset(child, 0, sizeof child); }
+  TrieNode() : n_words(0), n_prefixes(0) { }
 };
 
-template <size_t alphabet_size>
+template <int alphabet_size>
 struct Trie {
-  static constexpr size_t npos = -1;
+  static constexpr int npos = -1;
   using TNode = TrieNode<alphabet_size>;
   vector<TNode> nodes;
 
@@ -21,10 +21,9 @@ struct Trie {
   ** Maps the given char to an unsigned integer
   ** inside the range [0..alphabet_size)
   */
-  size_t char_to_child(char c) {
-    size_t result = c - '0';
-
-    assert(0 <= result && result < alphabet_size);
+  int char_to_child(char c) {
+    int result = c - '0';
+    //assert(0 <= result && result < alphabet_size);
     return result;
   }
 
@@ -32,15 +31,14 @@ struct Trie {
   ** Adds the given word to the trie
   */
   void add_word(const char *s) {
-    size_t current = 0;
+    int current = 0;
 
     for (int i = 0; s[i]; i++) {
       nodes[current].n_prefixes += 1;
 
-      size_t next_child = char_to_child(s[i]);
-      assert(next_child >= 0);
+      int next_child = char_to_child(s[i]);
 
-      size_t next_node = nodes[current].child[next_child];
+      int next_node = nodes[current].child[next_child];
       if (next_node == 0) {
         next_node = nodes.size();
         nodes[current].child[next_child] = next_node;
@@ -58,13 +56,13 @@ struct Trie {
   ** Returns the node ID where the traversal stopped, or
   ** Trie::npos if it couldn't follow the whole string.
   */
-  size_t traverse(const char *s) {
-    size_t current = 0;
+  int traverse(const char *s) {
+    int current = 0;
 
     for (int i = 0; s[i]; i++) {
-      size_t next_child = char_to_child(s[i]);
+      int next_child = char_to_child(s[i]);
 
-      size_t next_node = nodes[current].child[next_child];
+      int next_node = nodes[current].child[next_child];
       if (next_node == 0) {
         return Trie::npos;
       }
@@ -75,15 +73,15 @@ struct Trie {
     return current;
   }
 
-  size_t count_prefixes(const char *s) {
-    size_t node = traverse(s);
-    size_t result = (node == Trie::npos ? 0 : nodes[node].n_prefixes);
+  int count_prefixes(const char *s) {
+    int node = traverse(s);
+    int result = (node == Trie::npos ? 0 : nodes[node].n_prefixes);
     return result;
   }
 
-  size_t count_words(const char *s) {
-    size_t node = traverse(s);
-    size_t result = (node == Trie::npos ? 0 : nodes[node].n_words);
+  int count_words(const char *s) {
+    int node = traverse(s);
+    int result = (node == Trie::npos ? 0 : nodes[node].n_words);
     return result;
   }
 };
