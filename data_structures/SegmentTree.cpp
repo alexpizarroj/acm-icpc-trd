a0bc4d127e780dd56e661308f90b4812835910ea
Alex Pizarro <alex.pizarro.j@gmail.com>
2015-07-12 15:03:17 -0400

    Updated Segment Tree

--- data_structures/SegmentTree.cpp
+++ data_structures/SegmentTree.cpp
@@ -1,3 +1,5 @@
+// Max Value Segment Tree + Range Update with Lazy Propagation
+
 struct Node {
   int s, e;       // Interval covered by this node
   int son[2];     // Children of this node
@@ -7,7 +9,9 @@ struct Node {
 struct SegmentTree {
   vector<Node> T;
 
-  SegmentTree(int N) {
+  SegmentTree(const vector<int>& vec) {
+    const int N = vec.size();
+    
     Node n;
     n.s = 0, n.e = N;
     n.son[0] = n.son[1] = -1;
@@ -15,12 +19,21 @@ struct SegmentTree {
 
     T.reserve(4 * N);
     T.push_back(n);
-    Init(0);
+    init(0, vec);
   }
+  
+  explicit SegmentTree(int N, int val) : SegmentTree(vector<int>(N, val)) {}
+  
+  explicit SegmentTree(int N) : SegmentTree(N, 0) {}
 
-  void Init(int u) {
+  void init(int u, const vector<int>& vec) {
     Node &n = T[u];
-    if (n.e - n.s == 1) return;
+    if (n.e - n.s == 1)
+    {
+      n.val = vec[n.s];
+      return;
+    }
+    
     int mid = (n.s + n.e) / 2;
     for (int i = 0; i < 2; i++) {
       Node c(n);
@@ -32,47 +45,52 @@ struct SegmentTree {
       }
       n.son[i] = T.size();
       T.push_back(c);
-      Init(n.son[i]);
+      init(n.son[i], vec);
     }
+    
+    n.val = max(T[n.son[0]].val, T[n.son[1]].val);
   }
 
-  void Update(int u, int le, int ri, int val) {
+  void update(int u, int le, int ri, int val) {
     Node &n = T[u];
     if (n.e <= le || n.s >= ri) return;
 
     if (n.s == le && n.e == ri) {
-      n.lazy |= val;
+      n.lazy = max(n.lazy, val);
       return;
     }
 
     if (le < T[n.son[0]].e) {
-      Update(n.son[0], le, min(T[n.son[0]].e, ri), val);
-      Update(n.son[1], max(T[n.son[1]].s, le), ri, val);
+      update(n.son[0], le, min(T[n.son[0]].e, ri), val);
+      update(n.son[1], max(T[n.son[1]].s, le), ri, val);
     } else {
-      Update(n.son[1], max(T[n.son[1]].s, le), ri, val);
+      update(n.son[1], max(T[n.son[1]].s, le), ri, val);
     }
-    n.val |= val;
+    n.val = max(n.val, val);
   }
 
-  void PushLazy(int u) {
+  void push_lazy(int u) {
     Node &n = T[u];
     if (n.lazy == 0) return;
 
-    n.val |= n.lazy;
-    for (int i = 0; i < 2; i++)
-      if (n.son[i] != -1) T[n.son[i]].lazy |= n.lazy;
+    n.val = max(n.val, n.lazy);
+    for (int i = 0; i < 2; i++) {
+      if (n.son[i] != -1) {
+        T[n.son[i]].lazy = max(T[n.son[i]].lazy, n.lazy);
+      }
+    }
     n.lazy = 0;
   }
 
-  int Query(int u, int le, int ri) {
+  int query(int u, int le, int ri) {
     Node &n = T[u];
     if (n.e <= le || n.s >= ri) return 0;
 
-    PushLazy(u);
+    push_lazy(u);
     if (n.s == le && n.e == ri) return n.val;
 
-    int res = Query(n.son[0], le, min(T[n.son[0]].e, ri));
-    res |= Query(n.son[1], max(T[n.son[1]].s, le), ri);
+    int res = query(n.son[0], le, min(T[n.son[0]].e, ri));
+    res = max(res, query(n.son[1], max(T[n.son[1]].s, le), ri));
     return res;
   }
 };
