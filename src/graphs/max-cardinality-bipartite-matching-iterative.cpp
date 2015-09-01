e1e0a7d9113f1e67fc0fda9f0a7492b1a9aee8a8
Alex Pizarro <alex.pizarro.j@gmail.com>
2015-08-31 23:42:02 -0400

    MCBM Iterative version is now in a struct and has more meaningful comments

--- src/graphs/max-cardinality-bipartite-matching-iterative.cpp
+++ src/graphs/max-cardinality-bipartite-matching-iterative.cpp
@@ -1,47 +1,52 @@
-typedef vector<int> vi;
-vector<vi> lst;  // adj list for left elements
-vi lema, rima;   // lema = left elements match, rima = right elements match
-int n, m;        // n = amount of left elements, m = amount of right elements
+struct MCBM {
+  // n = # of left elements, m = # of right elements
+  int n, m;
+  // adj list for left elements
+  // left elements are [0..n-1], right elements are [0..m-1]
+  vector<vector<int> > lst;
 
-bool find_match(int s) {
-  vi from(n, -1);
-  queue<int> q;
-  int where, match, next;
-  bool found = false;
+  bool find_match(int s, vector<int>& lema, vector<int>& rima) {
+    vector<int> from(n, -1);
+    queue<int> q;
+    int where, match, next;
+    bool found = false;
 
-  q.push(s), from[s] = s;
-  while (!found && !q.empty()) {
-    where = q.front();
-    q.pop();
-    for (int i = 0; i < (int)lst[where].size(); ++i) {
-      match = lst[where][i];
-      next = rima[match];
-      if (where != next) {
-        if (next == -1) {
-          found = true;
-          break;
+    q.push(s), from[s] = s;
+    while (!found && !q.empty()) {
+      where = q.front();
+      q.pop();
+      for (int i = 0; i < (int)lst[where].size(); ++i) {
+        match = lst[where][i];
+        next = rima[match];
+        if (where != next) {
+          if (next == -1) {
+            found = true;
+            break;
+          }
+          if (from[next] == -1) q.push(next), from[next] = where;
         }
-        if (from[next] == -1) q.push(next), from[next] = where;
       }
     }
-  }
 
-  if (found) {
-    while (from[where] != where) {
-      next = lema[where];
+    if (found) {
+      while (from[where] != where) {
+        next = lema[where];
+        lema[where] = match, rima[match] = where;
+        where = from[where], match = next;
+      }
       lema[where] = match, rima[match] = where;
-      where = from[where], match = next;
     }
-    lema[where] = match, rima[match] = where;
+
+    return found;
   }
 
-  return found;
-}
-
-int maximum_matching()  // O(V*E)
-{
-  int ans = 0;
-  lema.assign(n, -1), rima.assign(m, -1);
-  for (int i = 0; i < n; ++i) ans += find_match(i);
-  return ans;
-}
+  int maximum_matching()  // O(V*E)
+  {
+    int ans = 0;
+    vector<int> lema(n, -1), rima(m, -1);
+    for (int i = 0; i < n; ++i) {
+      ans += find_match(i, lema, rima);
+    }
+    return ans;
+  }
+};
