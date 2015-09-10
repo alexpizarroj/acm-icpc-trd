04cdd5a43e5fa9ed1e16f4315e361de1ba4b13f3
Alex Pizarro <alex.pizarro.j@gmail.com>
2015-09-10 00:46:14 -0400

    Improved Edmonds-Karp support for multiple edges by replacing the matrix representation with an edge list

--- src/graphs/edmonds-karp.cpp
+++ src/graphs/edmonds-karp.cpp
@@ -1,43 +1,55 @@
-const int MAX = 400;
-vector<vector<int> > adjlist;
-int res[MAX][MAX], n, s, t;
+struct MfEdge {
+  int v, cap;
+  int backid; // id to the back edge
+};
 
-int find_path() {
-  const int inf = int(1e9 + 7);
-  vector<int> visited(n, 0), from(n, -1);
-  queue<int> q;
-  int u, v, f;
+struct MaxFlow {
+  vector<vector<int>> g;  // integers represent edges' ids
+  vector<MfEdge> edges;   // edges.size() should always be even
+  int n, s, t;            // n = # vertices, s = src vertex, t = sink vertex
 
-  q.push(s);
-  visited[s] = true;
-  while (!visited[t] && !q.empty()) {
-    u = q.front();
-    q.pop();
-    for (int i = 0; i < (int)adjlist[u].size(); ++i) {
-      v = adjlist[u][i];
-      if (res[u][v] > 0 && !visited[v]) {
-        from[v] = u;
-        q.push(v);
-        visited[v] = true;
-        if (v == t) {
-          break;
+  int find_path() {
+    const int inf = int(1e9 + 7);
+    vector<int> visited(n, 0), from(n, -1), used_edge(n, -1);
+    queue<int> q;
+    int u, v, f;
+
+    q.push(s);
+    visited[s] = true;
+    while (!visited[t] && !q.empty()) {
+      u = q.front();
+      q.pop();
+      for (int eid : g[u]) {
+        int v = edges[eid].v;
+        if (edges[eid].cap > 0 && !visited[v]) {
+          from[v] = u, used_edge[v] = eid;
+          q.push(v);
+          visited[v] = true;
+          if (v == t) {
+            break;
+          }
         }
       }
     }
+
+    f = inf;
+    if (visited[t]) {
+      for (v = t; from[v] > -1; v = from[v]) {
+        f = min(edges[used_edge[v]].cap, f);
+      }
+      for (v = t; from[v] > -1; v = from[v]) {
+        int backid = edges[used_edge[v]].backid;
+        edges[used_edge[v]].cap -= f;
+        edges[backid].cap += f;
+      }
+    }
+
+    return (f == inf ? 0 : f);
   }
 
-  f = inf;
-  if (visited[t]) {
-    for (v = t; from[v] > -1; v = from[v]) f = min(res[from[v]][v], f);
-    for (v = t; from[v] > -1; v = from[v])
-      u = from[v], res[u][v] -= f, res[v][u] += f;
+  int get() {
+    int mf = 0, d;
+    while ((d = find_path())) mf += d;
+    return mf;
   }
-
-  return (f == inf ? 0 : f);
-}
-
-int maxflow() {
-  int mf = 0, d;
-  while ((d = find_path())) mf += d;
-  return mf;
-}
+};
