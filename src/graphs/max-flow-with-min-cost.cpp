716031cbae115ecff1812f7fe456b6168bd10c74
Alex Pizarro <alex.pizarro.j@gmail.com>
2015-09-18 08:06:50 -0400

    Updated MinCostMaxFlow to use SPFA

--- src/graphs/max-flow-with-min-cost.cpp
+++ src/graphs/max-flow-with-min-cost.cpp
@@ -1,32 +1,38 @@
-struct MfEdge {
-  int v, cap, cpu;
-  int backid; // id to the back edge
-};
-
-struct FlowResult {
-  int flow, cost;
-};
-
 struct McMaxFlow {
+  struct MfEdge { int v, cap, cpu, backid; };
+  struct FlowResult { int flow, cost; };
   vector<vector<int>> g;  // integers represent edges' ids
   vector<MfEdge> edges;   // edges.size() should always be even
   int n, s, t;            // n = # vertices, s = src vertex, t = sink vertex
+  
+  // Directed Edge u - > v with capacity 'cap' and cost per unit 'cpu'
+  void add_edge(int u, int v, int cap, int cpu) {
+    int eid = edges.size();
+    g[u].push_back(eid);
+    g[v].push_back(eid + 1);
+    edges.push_back((MfEdge){v, cap, cpu, eid + 1});
+    edges.push_back((MfEdge){u, 0, -cpu, eid});
+  }
 
   FlowResult find_path() {
     const int inf = int(1e9 + 7);
     vector<int> from(n, -1), used_edge(n, -1);
     
     vector<int> dist(n, inf);
-    dist[s] = 0;
-    for (int iter = 1; iter < n; ++iter) {
-      for (int u = 0; u < n; ++u) {
-        for (int eid : g[u]) {
-          int v = edges[eid].v;
-          int cand_dist = dist[u] + edges[eid].cpu;
-          if (edges[eid].cap > 0 && cand_dist < dist[v]) {
-            dist[v] = cand_dist;
-            from[v] = u; used_edge[v] = eid;
-          }
+    queue<int> q; vector<bool> queued(n, false);
+    dist[s] = 0; q.push(s); queued[s] = true;
+    
+    while (!q.empty()) {
+      const int u = q.front(); q.pop();
+      queued[u] = false;
+      
+      for (int eid : g[u]) {
+        int v = edges[eid].v;
+        int cand_dist = dist[u] + edges[eid].cpu;
+        if (edges[eid].cap > 0 && cand_dist < dist[v]) {
+          dist[v] = cand_dist;
+          from[v] = u; used_edge[v] = eid;
+          if (!queued[v]) { q.push(v); queued[v] = true; }
         }
       }
     }
